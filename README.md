600436fd3b7a377acf60effed377091c327ca21f
BennettJames <bennett.sala@gmail.com>
2022-05-27 23:19:27 -0700

    added readme

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# ef, a bad generics library for Go
+
+This is a very silly generic-focused utility library for Go. Contains many bad
+ideas, and maybe a few good ones that will be better implemented by other
+people.
--- errors.go
+++ errors.go
@@ -34,3 +34,77 @@ func (e *UnreachableError) Error() string {
 
 	return "unreachable"
 }
+
+func Recover(errAddr *error) {
+	if errAddr == nil {
+		panic("Recover called with nil result reference")
+	}
+
+	switch narrowed := recover().(type) {
+	case nil:
+		// ques [bs]: is doing this in a type switch less efficient then just checking
+		// directly?
+		return
+	case error:
+		*errAddr = narrowed
+	default:
+		*errAddr = &RecoverError{recovered: narrowed}
+	}
+}
+
+func Catch(errAddr *error, recoverFn func(error) error) {
+	if errAddr == nil {
+		panic("Recover called with nil result reference")
+	}
+
+	var recoveredErr error
+	switch narrowed := recover().(type) {
+	case nil:
+		// ques [bs]: is doing this in a type switch less efficient then just checking
+		// directly?
+		return
+	case error:
+		recoveredErr = narrowed
+	default:
+		recoveredErr = &RecoverError{recovered: narrowed}
+	}
+	*errAddr = recoverFn(recoveredErr)
+}
+
+func Try[T any](v T, err error) T {
+	if err != nil {
+		// todo [bs]: probably should wrap this and unwrap in a recover
+		panic(err)
+	}
+	return v
+}
+
+func Try2[T, U any](t T, u U, err error) (T, U) {
+	if err != nil {
+		// todo [bs]: probably should wrap this and unwrap in a recover
+		panic(err)
+	}
+	return t, u
+}
+
+func Try3[T, U, V any](t T, u U, v V, err error) (T, U, V) {
+	if err != nil {
+		// todo [bs]: probably should wrap this and unwrap in a recover
+		panic(err)
+	}
+	return t, u, v
+}
+
+func Assert(check bool, msg string) {
+	if !check {
+		// todo [bs]: wrap this
+		panic(msg)
+	}
+}
+
+func Assertf(check bool, msg string, a ...any) {
+	if !check {
+		// todo [bs]: wrap this better
+		panic(fmt.Errorf(msg, a...))
+	}
+}
